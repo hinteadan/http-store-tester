96dabd0879d3f84efda707bebc4b6e7a3ebd5fdf
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-05-18 20:18:48 +0300

    UI for blotter/add/remove entity

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,13 @@
 (function(angular){
 	'use strict';
 
-	var hds = this.H.DataStore,
-		log = this.console.log;
+	var confirm = this.confirm,
+		hds = this.H.DataStore,
+		resultsLog = [],
+		log = function(d){
+			console.log(d);
+			resultsLog.push(d);
+		};
 
 	angular.module('HttpDataStoreTester', [])
 	.constant('storeUrl', 'http://h-httpstore.azurewebsites.net/')
@@ -10,8 +15,32 @@
 		return new hds.Store('HttpDataStoreTester', storeUrl);
 	}])
 	.controller('TesterCtrl', ['$scope', 'store', function($scope, store){
-		store.QueryMeta(hds.queryWithOr()).then(log);
+		$scope.createEntity = function(){
+			$scope.newEntity = new hds.Entity({});
+		};
+		$scope.storeEntity = function(){
+			store.Save($scope.newEntity).then(function(result){
+				$scope.newEntity = null;
+				log(result);
+			});
+		};
+		$scope.delete = function(entityId){
+			if(!confirm('Are you sure you want to remove the selected entity?')){
+				return;
+			}
+			store.Delete(entityId).then($scope.refreshBlotter);
+		};
+		$scope.refreshBlotter = function(){
+			store.QueryMeta(hds.queryWithAnd()).then(function(result){
+				$scope.entities = result.data;
+				log(result);
+				$scope.$apply();
+			});
+		};
+
+		$scope.refreshBlotter();
 		$scope.entity = {"Name": "Hintee"};
+		$scope.log = resultsLog;
 	}]);
 
 
