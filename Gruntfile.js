c1fdc4a72ac9ce655ecb57ba34c2ccbee874ae06
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-05-18 17:12:49 +0300

    Add project to git

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+// Generated on 2014-04-06 using generator-angular 0.8.0
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      js: {
+        files: ['app/scripts/{,*/}*.js'],
+        options: {
+          livereload: true
+        }
+      },
+      styles: {
+        files: ['app/styles/{,*/}*.css']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          'app/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '.tmp',
+            'app'
+          ]
+        }
+      }
+    }
+  });
+
+
+  grunt.registerTask('serve', function (target) {
+    grunt.task.run([
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
+
+  grunt.registerTask('default', ['serve']);
+};
---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+(function(angular){
+	'use strict';
+
+	var hds = this.H.DataStore,
+		log = this.console.log;
+
+	angular.module('HttpDataStoreTester', [])
+	.constant('storeUrl', 'http://h-httpstore.azurewebsites.net/')
+	.factory('store', ['storeUrl', function(storeUrl){
+		return new hds.Store('HttpDataStoreTester', storeUrl);
+	}])
+	.controller('TesterCtrl', ['$scope', 'store', function($scope, store){
+		store.QueryMeta(hds.queryWithOr()).then(log);
+	}]);
+
+
+}).call(this, this.angular);
